501b0d248828ff2fbac56f5179fd55326d05fc21
Jenney <jenney.ren@mail.utoronto.ca>
2018-11-18 00:43:58 -0500

    Add BattleScoreboardActivity

---
+++ Phase2/GameCentre/app/src/main/java/battlegame/BattleScoreboardActivity.java
@@ -0,0 +1,10 @@
+package battlegame;
+
+import android.support.v7.app.AppCompatActivity;
+
+import gamecentre.Score;
+
+public class BattleScoreboardActivity extends AppCompatActivity {
+    static Score scores;
+    BattleScoreboard scoreboard;
+}
