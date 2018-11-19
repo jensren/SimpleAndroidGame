8c6d5e95ac57d8b9f9f5e6801378c92291f32257
Andrew Jiang <andrew98jiang@gmail.com>
2018-11-19 14:35:50 -0500

    Began writing matchinggameaactivity

--- Phase2/GameCentre/app/src/main/java/cardmatching/MatchingGameActivity.java
+++ Phase2/GameCentre/app/src/main/java/cardmatching/MatchingGameActivity.java
@@ -1,4 +1,41 @@
 package cardmatching;
 
-public class MatchingGameActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Observer;
+
+import slidingtiles.CustomAdapter;
+
+public class MatchingGameActivity extends AppCompatActivity implements Observer{
+    /**
+     * The board manager.
+     */
+    private MatchingBoardManager boardManager;
+
+    /**
+     * The buttons to display.
+     */
+    private ArrayList<Button> tileButtons;
+
+    // Grid View and calculated column height and width based on device size
+    private MatchingGestureDetectGridView gridView;
+    private static int columnWidth, columnHeight;
+
+    /**
+     * Set up the background image for each button based on the master list
+     * of positions, and then call the adapter to set the view.
+     */
+    public void display() {
+        updateTileButtons();
+        gridView.setAdapter(new CustomAdapter(tileButtons, columnWidth, columnHeight));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+    }
 }
