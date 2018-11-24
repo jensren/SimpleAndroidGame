7394ab59365991ad50c1a19d09915b7bc1b7b92a
cw <wangcassidy@gmail.com>
2018-11-24 17:25:01 -0500

    Started animations in activities

--- Phase2/GameCentre/app/src/main/java/gamecentre/battlegame/Player1DogChoiceActivity.java
+++ Phase2/GameCentre/app/src/main/java/gamecentre/battlegame/Player1DogChoiceActivity.java
@@ -1,10 +1,12 @@
 package gamecentre.battlegame;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import gamecentre.slidingtiles.R;
 
@@ -22,6 +24,31 @@ public class Player1DogChoiceActivity extends AppCompatActivity {
         addDruidButtonListener();
         addSirShibeButtonListener();
 
+        startAnimations();
+
+    }
+
+    /**
+     * Start the animations in the activity.
+     */
+    private void startAnimations() {
+        AnimationDrawable druidAnimation;
+        ImageView ninjaImage = findViewById(R.id.animateddruid1);
+        ninjaImage.setBackgroundResource(R.drawable.druid_shibe_animated);
+        druidAnimation = (AnimationDrawable) ninjaImage.getBackground();
+        druidAnimation.start();
+
+        AnimationDrawable sirAnimation;
+        ImageView shamanImage = findViewById(R.id.animatedsir1);
+        shamanImage.setBackgroundResource(R.drawable.sir_shibe_animated);
+        sirAnimation = (AnimationDrawable) shamanImage.getBackground();
+        sirAnimation.start();
+
+        AnimationDrawable detectiveAnimation;
+        ImageView samuraiImage = findViewById(R.id.animateddetective1);
+        samuraiImage.setBackgroundResource(R.drawable.detective_shibe_animated);
+        detectiveAnimation = (AnimationDrawable) samuraiImage.getBackground();
+        detectiveAnimation.start();
     }
 
     /**
