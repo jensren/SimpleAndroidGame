f69c7a5e3576955b4d11c935356ec8b0fc3b9264
ashasmemon <ashas.memon@mail.utoronto.ca>
2018-11-25 15:36:12 -0500

    Added in updatePlayerAttributesStack into each of the character's regularMove and specialMove.

--- Phase2/GameCentre/app/src/main/java/gamecentre/battlegame/SirShibe.java
+++ Phase2/GameCentre/app/src/main/java/gamecentre/battlegame/SirShibe.java
@@ -16,6 +16,7 @@ public class SirShibe extends Character {
 
     @Override
     void regularMove() {
+        getBattleQueue().updatePlayerAttributesStack(this);
         getBattleQueue().updateUndoStack(getBattleQueue().copyBq());
         this.getOpponent().reduceHp(REGULAR_MOVE_DAMAGE);
         getBattleQueue().add(this);
@@ -27,28 +28,19 @@ public class SirShibe extends Character {
      */
     @Override
     void specialMove() {
+        getBattleQueue().updatePlayerAttributesStack(this);
         getBattleQueue().updateUndoStack(getBattleQueue().copyBq());
         Character ch1 = getBattleQueue().getNextCharacter();
         BattleQueue bq = ch1.getBattleQueue();
-        Character ch;
 
         reduceMp(SPECIAL_MOVE_COST);
         this.getOpponent().reduceHp(SPECIAL_MOVE_DAMAGE);
 
-        int p1Count = 0;
-        int p2Count = 0;
-
         while (!bq.isEmpty()) {
-            ch = bq.removeCharacter();
-            if (ch == ch1) { p1Count += 1;}
-            else {p2Count += 1;}
+            bq.removeCharacter();
         }
-        if (p1Count > 0) {
-            bq.add(ch1);
-        }
-        if (p2Count > 0) {
-            bq.add(ch1.getOpponent());
-        }
+        bq.add(ch1);
+        bq.add(ch1.getOpponent());
 
     }
 
