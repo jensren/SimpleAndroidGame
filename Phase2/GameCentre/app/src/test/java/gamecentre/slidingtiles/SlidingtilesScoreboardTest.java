2cfc04ab0734217d6d329cc37c4a44fdee964513
Jenney <jenney.ren@mail.utoronto.ca>
2018-11-24 19:48:27 -0500

    Fix SlidingScoreboardTest so that tests work correctly

--- Phase2/GameCentre/app/src/test/java/gamecentre/slidingtiles/SlidingtilesScoreboardTest.java
+++ Phase2/GameCentre/app/src/test/java/gamecentre/slidingtiles/SlidingtilesScoreboardTest.java
@@ -7,14 +7,15 @@ import java.util.HashMap;
 import gamecentre.Score;
 import gamecentre.Scoreboard;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SlidingtilesScoreboardTest {
 
     /**
      * The SlidingtilesScoreboard to test
      */
-    SlidingtilesScoreboard scoreboard = new SlidingtilesScoreboard();
+    private SlidingtilesScoreboard scoreboard = new SlidingtilesScoreboard();
 
     /**
      * Create a generic score list, where Player i is in ith place, with a score of i
@@ -60,8 +61,12 @@ public class SlidingtilesScoreboardTest {
     @Test
     public void testGetUserCurrentScoreWithScore() {
         populateScoreboard();
-        SlidingtilesScoreboard.setUser("Player 1");
-        assertEquals("Your Score: 1", scoreboard.getUserCurrentScore());
+        SlidingtilesScoreboard.setUser("1");
+        SlidingtilesScoreboard.setNumMoves(100);
+        SlidingtilesScoreboard.setBoardSize(1);
+        // The player's score will be 100
+        scoreboard.update();
+        assertEquals("Your Score: 100", scoreboard.getUserCurrentScore());
     }
 
     /**
@@ -80,7 +85,7 @@ public class SlidingtilesScoreboardTest {
     @Test
     public void getUserBestScoreWithScore() {
         populateScoreboard();
-        SlidingtilesScoreboard.setUser("Player 1");
+        SlidingtilesScoreboard.setUser("1");
         assertEquals("Best Score: 1", scoreboard.getUserBestScore());
     }
 
@@ -100,10 +105,10 @@ public class SlidingtilesScoreboardTest {
     @Test
     public void testUpdateNotHighScore() {
         populateScoreboard();
-        SlidingtilesScoreboard.setUser("Player 1");
-        SlidingtilesScoreboard.setNumMoves(1);
+        SlidingtilesScoreboard.setUser("1");
+        SlidingtilesScoreboard.setNumMoves(100);
         SlidingtilesScoreboard.setBoardSize(1);
-        // Score will be 1
+        // The player's score will be 100
         scoreboard.update();
         Score[] expectedGameHighScores = createScoreList();
         HashMap<String, Score> expectedUserToBestScore = createUserToBestScores(expectedGameHighScores);
@@ -121,10 +126,12 @@ public class SlidingtilesScoreboardTest {
     public void testUpdateNewPlayer() {
         populateScoreboard();
         SlidingtilesScoreboard.setUser("New Player");
-        Score newScore = new Score("New Player", 100);
-        scoreboard.setCurrentScore(newScore);
+        SlidingtilesScoreboard.setNumMoves(100);
+        SlidingtilesScoreboard.setBoardSize(1);
+        // The player's score will be 100
         scoreboard.update();
 
+        Score newScore = new Score("New Player", 100);
         Score[] expectedGameHighScores = createScoreList();
         HashMap<String, Score> expectedUserToBestScore = createUserToBestScores(expectedGameHighScores);
         expectedUserToBestScore.put("New Player", newScore);
@@ -143,20 +150,21 @@ public class SlidingtilesScoreboardTest {
     public void testUpdateNewHighScore() {
         populateScoreboard();
         SlidingtilesScoreboard.setUser("Underdog");
-        Score oldScore = new Score("Underdog", 100);
-        scoreboard.setCurrentScore(oldScore);
+        SlidingtilesScoreboard.setNumMoves(100);
+        SlidingtilesScoreboard.setBoardSize(1);
+        // The player's score will be 100
         scoreboard.update();
         Score newScore = new Score("Underdog", 0);
-        scoreboard.setCurrentScore(newScore);
+        SlidingtilesScoreboard.setNumMoves(1);
+        SlidingtilesScoreboard.setBoardSize(100);
+        // The player's score will be 0
         scoreboard.update();
 
         Score[] s = createScoreList();
+        HashMap<String, Score> expectedUserToBestScore = createUserToBestScores(s);
         Score[] expectedGameHighScores = new Score[Scoreboard.LENGTH];
         expectedGameHighScores[0] = newScore;
-        for (int i = 0; i < Scoreboard.LENGTH - 1; i++) {
-            expectedGameHighScores[i + 1] = s[i];
-        }
-        HashMap<String, Score> expectedUserToBestScore = createUserToBestScores(expectedGameHighScores);
+        System.arraycopy(s, 0, expectedGameHighScores, 1, Scoreboard.LENGTH - 1);
         expectedUserToBestScore.put("Underdog", newScore);
 
         Score[] actualGameHighScores = scoreboard.getScoreList();
