5c11c35a4ec0c8d89fb9c1b424a1d0729e59b32e
cw <wangcassidy@gmail.com>
2018-11-26 15:23:36 -0500

    Adding saving and loading functionality

--- Phase2/GameCentre/app/src/main/java/gamecentre/battlegame/BattleStartingActivity.java
+++ Phase2/GameCentre/app/src/main/java/gamecentre/battlegame/BattleStartingActivity.java
@@ -32,10 +32,6 @@ public class BattleStartingActivity extends AppCompatActivity {
      */
     public static String tempSaveFileName;
     /**
-     * The auto saved file.
-     */
-    public static String autoSaveFileName;
-    /**
      * The battle queue.
      */
     private BattleQueue battleQueue;
@@ -95,7 +91,6 @@ public class BattleStartingActivity extends AppCompatActivity {
             public void onClick(View v) {
                 loadFromFile(saveFileName);
                 saveToFile(tempSaveFileName);
-                saveToFile(autoSaveFileName);
                 makeToastLoadedText();
                 switchToGameActivity();
             }
@@ -136,7 +131,7 @@ public class BattleStartingActivity extends AppCompatActivity {
      */
     private void switchToCatChoiceActivity() {
         Intent tmp = new Intent(this, CatOrDogActivity.class);
-        //saveToFile(SlidingtilesStartingActivity.tempSaveFileName);
+        saveToFile(BattleStartingActivity.tempSaveFileName);
         startActivity(tmp);
     }
 
@@ -144,7 +139,7 @@ public class BattleStartingActivity extends AppCompatActivity {
      * Switch to the BattleGame view.
      */
     private void switchToGameActivity() {
-        Intent tmp = new Intent(this, CatOrDogActivity.class);
+        Intent tmp = new Intent(this, BattleGameActivity.class);
         saveToFile(BattleStartingActivity.tempSaveFileName);
         startActivity(tmp);
     }
