190e1c07d70938bdf919121ee261cc143488058e
cw <wangcassidy@gmail.com>
2018-11-26 15:21:35 -0500

    Removed autosave variable from the battle game

--- Phase2/GameCentre/app/src/main/java/gamecentre/SignUpActivity.java
+++ Phase2/GameCentre/app/src/main/java/gamecentre/SignUpActivity.java
@@ -79,7 +79,6 @@ public class SignUpActivity extends AppCompatActivity {
         SlidingtilesStartingActivity.autoSaveFileName = username + "_slidingtiles_autosave.ser";
         SlidingtilesStartingActivity.tempSaveFileName = username + "_slidingtiles_tmp.ser";
         BattleStartingActivity.saveFileName = username + "_battlegame.ser";
-        BattleStartingActivity.autoSaveFileName = username + "_battlegame_autosave.ser";
         BattleStartingActivity.tempSaveFileName = username + "_battlegame_tmp.ser";
         MatchingStartingActivity.matchingSaveFileName = username + "_matching.ser";
         MatchingStartingActivity.matchingAutoSaveFileName = username + "_matching_autosave.ser";
