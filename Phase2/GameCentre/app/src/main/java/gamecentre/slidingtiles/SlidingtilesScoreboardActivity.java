520807b88e04d6f184f6347d1f72f88ff45a26e7
cw <wangcassidy@gmail.com>
2018-11-28 19:58:38 -0500

    Moved saving and loading methods to Serializer

--- Phase2/GameCentre/app/src/main/java/gamecentre/slidingtiles/SlidingtilesScoreboardActivity.java
+++ Phase2/GameCentre/app/src/main/java/gamecentre/slidingtiles/SlidingtilesScoreboardActivity.java
@@ -3,37 +3,48 @@ package gamecentre.slidingtiles;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import gamecentre.Serializer;
 
 /**
  * Manage the scoreBoard layout during the game and save the current scoreBoard.
  */
 
 public class SlidingtilesScoreboardActivity extends AppCompatActivity {
+
+    /**
+     * The textview for the scoreboard.
+     */
     TextView scoreBoard;
+    /**
+     * The file to save scoreboard to.
+     */
     public static final String SCORE_FILENAME = "slidingtiles_scoreboard_file.ser";
+
+    /**
+     * The scoreboard.
+     */
     SlidingtilesScoreboard scoreboard;
+
+    /**
+     * The serializer for this activity.
+     */
+    Serializer serializer = new Serializer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scoreboard);
         scoreBoard = findViewById(R.id.s_b);
-        loadFromFile(SCORE_FILENAME);
+        scoreboard = (SlidingtilesScoreboard) serializer.loadScoreboardFromFile(SCORE_FILENAME);
         if (scoreboard == null) {
             scoreboard = new SlidingtilesScoreboard();
         }
         scoreboard.update();
-        saveToFile(SCORE_FILENAME);
+        serializer.saveScoreboardToFile(SCORE_FILENAME, scoreboard);
         scoreBoard.setText(scoreboard.toString());
         displayScore();
         displayBestScore();
@@ -50,44 +61,7 @@ public class SlidingtilesScoreboardActivity extends AppCompatActivity {
     @Override
     public void onPause() {
         super.onPause();
-        saveToFile(SCORE_FILENAME);
-    }
-
-    /**
-     * Load a preexisting ScoreBoard from a previous game.
-     * @param fileName name of file to load
-     */
-    private void loadFromFile(String fileName) {
-        try {
-            InputStream inputStream = this.openFileInput(fileName);
-            if (inputStream != null) {
-                ObjectInputStream input = new ObjectInputStream(inputStream);
-                scoreboard = (SlidingtilesScoreboard) input.readObject();
-                inputStream.close();
-            }
-        } catch (FileNotFoundException e) {
-            Log.e("login activity", "File not found: " + e.toString());
-        } catch (IOException e) {
-            Log.e("login activity", "Can not read file: " + e.toString());
-        } catch (ClassNotFoundException e) {
-            Log.e("login activity", "File contained unexpected data type: " + e.toString());
-        }
-    }
-
-    /**
-     * Save the current scoreBoard to file for later access.
-     * @param fileName name of the file to save
-     */
-
-    public void saveToFile(String fileName) {
-        try {
-            ObjectOutputStream outputStream = new ObjectOutputStream(
-                    this.openFileOutput(fileName, MODE_PRIVATE));
-            outputStream.writeObject(scoreboard);
-            outputStream.close();
-        } catch (IOException e) {
-            Log.e("Exception", "File write failed: " + e.toString());
-        }
+        serializer.saveScoreboardToFile(SCORE_FILENAME, scoreboard);
     }
 
     /**
