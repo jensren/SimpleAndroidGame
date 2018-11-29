6d8c8d58e27d42d68cc21cfc44d421139dd59d7f
cw <wangcassidy@gmail.com>
2018-11-29 18:13:14 -0500

    More file saving fixes

--- Phase2/GameCentre/app/src/main/java/gamecentre/Serializer.java
+++ Phase2/GameCentre/app/src/main/java/gamecentre/Serializer.java
@@ -1,12 +1,10 @@
 package gamecentre;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
@@ -18,21 +16,24 @@ import gamecentre.slidingtiles.BoardManager;
 /**
  * Saves and loads (serializes) files.
  */
+@SuppressLint("Registered")
 public class Serializer extends AppCompatActivity {
 
     /**
      * Load the board manager from fileName.
      *
-     * @param fileName the name of the file
+     * @param filename the name of the file
+     * @param context the context of the activity
+     * @return the board manager
      */
-    public BoardManager loadBoardManagerFromFile(String fileName) {
+    public BoardManager loadBoardManagerFromFile(String filename, Context context) {
+        BoardManager tmpBoardManager = new BoardManager();
         try {
-            InputStream inputStream = new FileInputStream(fileName);
+            InputStream inputStream = context.openFileInput(filename);
             if (inputStream != null) {
                 ObjectInputStream input = new ObjectInputStream(inputStream);
-                BoardManager tmpBoardManager = (BoardManager) input.readObject();
+                tmpBoardManager = (BoardManager) input.readObject();
                 inputStream.close();
-
             }
         } catch (FileNotFoundException e) {
             Log.e("login activity", "File not found: " + e.toString());
@@ -41,20 +42,22 @@ public class Serializer extends AppCompatActivity {
         } catch (ClassNotFoundException e) {
             Log.e("login activity", "File contained unexpected data type: " + e.toString());
         }
-        return null;
+        return tmpBoardManager;
     }
 
     /**
      * Save the board manager to fileName.
      *
      * @param filename the name of the file
+     * @param context the context of the activity
+     * @param boardManager the board manager
      */
-    public void saveBoardManagerToFile(String filename, BoardManager boardManager) {
+    public void saveBoardManagerToFile(String filename, BoardManager boardManager, Context context) {
         try {
-            FileOutputStream fileOutputStream = new FileOutputStream(filename);
-            ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream);
+            ObjectOutputStream outputStream = new ObjectOutputStream(
+                    context.openFileOutput(filename, MODE_PRIVATE));
             outputStream.writeObject(boardManager);
-            fileOutputStream.close();
+            outputStream.close();
             outputStream.close();
         } catch (IOException e) {
             Log.e("Exception", "File write failed: " + e.toString());
@@ -64,15 +67,19 @@ public class Serializer extends AppCompatActivity {
     /**
      * Load a preexisting ScoreBoard from a previous game.
      *
-     * @param fileName name of file to load
+     * @param filename name of file to load
+     * @param context the context of the activity
+     * @return the scoreboard
      */
-    public Scoreboard loadScoreboardFromFile(String fileName) {
+    public Scoreboard loadScoreboardFromFile(String filename, Context context) {
+        Scoreboard tmpScoreboard = null;
         try {
-            InputStream inputStream = new FileInputStream(fileName);
-            ObjectInputStream input = new ObjectInputStream(inputStream);
-            Scoreboard tmpScoreboard = (Scoreboard) input.readObject();
-            inputStream.close();
-            return tmpScoreboard;
+            InputStream inputStream = context.openFileInput(filename);
+            if (inputStream != null) {
+                ObjectInputStream input = new ObjectInputStream(inputStream);
+                tmpScoreboard = (Scoreboard) input.readObject();
+                inputStream.close();
+            }
         } catch (FileNotFoundException e) {
             Log.e("login activity", "File not found: " + e.toString());
         } catch (IOException e) {
@@ -80,20 +87,22 @@ public class Serializer extends AppCompatActivity {
         } catch (ClassNotFoundException e) {
             Log.e("login activity", "File contained unexpected data type: " + e.toString());
         }
-        return null;
+        return tmpScoreboard;
     }
 
     /**
      * Save the current scoreBoard to file for later access.
      *
      * @param filename name of the file to save
+     * @param context the context of the activity
+     * @param scoreboard the scoreboard to save to file
      */
-    public void saveScoreboardToFile(String filename, Scoreboard scoreboard) {
+    public void saveScoreboardToFile(String filename, Scoreboard scoreboard, Context context) {
         try {
-            FileOutputStream fileOutputStream = new FileOutputStream(filename);
-            ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream);
+            ObjectOutputStream outputStream = new ObjectOutputStream(
+                    context.openFileOutput(filename, MODE_PRIVATE));
             outputStream.writeObject(scoreboard);
-            fileOutputStream.close();
+            outputStream.close();
             outputStream.close();
         } catch (IOException e) {
             Log.e("Exception", "File write failed: " + e.toString());
@@ -103,16 +112,18 @@ public class Serializer extends AppCompatActivity {
     /**
      * Load the board manager from fileName.
      *
-     * @param fileName the name of the file
+     * @param filename the name of the file
+     * @param context the context of the activity
+     * @return the matching board manager
      */
-    public MatchingBoardManager loadMatchingBoardManagerFromFile(String fileName) {
+    public MatchingBoardManager loadMatchingBoardManagerFromFile(String filename, Context context) {
+        MatchingBoardManager tmpBoardManager = new MatchingBoardManager();
         try {
-            InputStream inputStream = new FileInputStream(fileName);
+            InputStream inputStream = context.openFileInput(filename);
             if (inputStream != null) {
                 ObjectInputStream input = new ObjectInputStream(inputStream);
-                MatchingBoardManager tmpBoardManager = (MatchingBoardManager) input.readObject();
+                tmpBoardManager = (MatchingBoardManager) input.readObject();
                 inputStream.close();
-                return tmpBoardManager;
             }
         } catch (FileNotFoundException e) {
             Log.e("login activity", "File not found: " + e.toString());
@@ -121,19 +132,20 @@ public class Serializer extends AppCompatActivity {
         } catch (ClassNotFoundException e) {
             Log.e("login activity", "File contained unexpected data type: " + e.toString());
         }
-        return null;
+        return tmpBoardManager;
     }
 
     /**
      * Save the board manager to fileName.
      *
-     * @param fileName the name of the file
-     * @param context
+     * @param filename the name of the file
+     * @param context the context of the activity
+     * @param matchingBoardManager the matching board manager to save to file
      */
-    public void saveMatchingBoardManagerToFile(String fileName, MatchingBoardManager matchingBoardManager, Context context) {
+    public void saveMatchingBoardManagerToFile(String filename, MatchingBoardManager matchingBoardManager, Context context) {
         try {
-            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-            ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream);
+            ObjectOutputStream outputStream = new ObjectOutputStream(
+                    context.openFileOutput(filename, MODE_PRIVATE));
             outputStream.writeObject(matchingBoardManager);
             outputStream.close();
         } catch (IOException e) {
@@ -143,6 +155,10 @@ public class Serializer extends AppCompatActivity {
 
     /**
      * Load the user manager from USER_FILENAME.
+     *
+     * @param filename the name of the file to load to
+     * @param context the context of the activity
+     * @return the user manager
      */
     public static UserManager loadUserManagerFromFile(String filename, Context context) {
         UserManager tmpUserManager = new UserManager();
@@ -164,7 +180,11 @@ public class Serializer extends AppCompatActivity {
     }
 
     /**
-     * Save the user manager to USER_FILENAME.
+     * Save the user manager to filename
+     *
+     * @param filename the name of hte file
+     * @param userManager the userrmanager to save
+     * @param context the context of the activity
      */
     public static void saveUserManagerToFile(String filename, UserManager userManager, Context context) {
         try {
