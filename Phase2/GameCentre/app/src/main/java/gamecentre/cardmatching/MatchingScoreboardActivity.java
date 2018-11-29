33eda74f3be9d773f48a8f7f1dd966b3352f4732
cw <wangcassidy@gmail.com>
2018-11-28 20:59:58 -0500

    Moved save and load methods from MatchingScoreboardActivity to Serializer

--- Phase2/GameCentre/app/src/main/java/gamecentre/cardmatching/MatchingScoreboardActivity.java
+++ Phase2/GameCentre/app/src/main/java/gamecentre/cardmatching/MatchingScoreboardActivity.java
@@ -3,38 +3,45 @@ package gamecentre.cardmatching;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
+import gamecentre.Serializer;
 import gamecentre.slidingtiles.R;
     /**
-     * Manage the scoreBoard layout during the game and save the current scoreBoard.
+     * Manage the scoreboardView layout during the game and save the current scoreboardView.
      */
     public class MatchingScoreboardActivity extends AppCompatActivity {
-        TextView scoreBoard;
+        /**
+         * The TextView for scoreboard
+         */
+        TextView scoreboardView;
+        /**
+         * The name of the file to serialize the scoreboard to
+         */
         public static final String SCORE_FILENAME = "cardmatching_scoreboard_file.ser";
+        /**
+         * The scoreboard for the game
+         */
         MatchingScoreboard scoreboard;
+        /**
+         * The serializer for this activity
+         */
+        Serializer serializer = new Serializer();
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.scoreboard);
-            scoreBoard = findViewById(R.id.s_b);
-            loadFromFile(SCORE_FILENAME);
+            scoreboardView = findViewById(R.id.s_b);
+            scoreboard = (MatchingScoreboard) serializer.loadScoreboardFromFile(SCORE_FILENAME);
             if (scoreboard == null) {
                 scoreboard = new MatchingScoreboard();
             }
             scoreboard.update();
-            saveToFile(SCORE_FILENAME);
-            scoreBoard.setText(scoreboard.toString());
+            serializer.saveScoreboardToFile(SCORE_FILENAME, scoreboard);
+            scoreboardView.setText(scoreboard.toString());
             displayScore();
             displayBestScore();
             addMainButtonListener();
@@ -50,44 +57,7 @@ import gamecentre.slidingtiles.R;
         @Override
         public void onPause() {
             super.onPause();
-            saveToFile(SCORE_FILENAME);
-        }
-
-        /**
-         * Load a preexisting ScoreBoard from a previous game.
-         * @param fileName name of file to load
-         */
-        private void loadFromFile(String fileName) {
-            try {
-                InputStream inputStream = this.openFileInput(fileName);
-                if (inputStream != null) {
-                    ObjectInputStream input = new ObjectInputStream(inputStream);
-                    scoreboard = (MatchingScoreboard) input.readObject();
-                    inputStream.close();
-                }
-            } catch (FileNotFoundException e) {
-                Log.e("login activity", "File not found: " + e.toString());
-            } catch (IOException e) {
-                Log.e("login activity", "Can not read file: " + e.toString());
-            } catch (ClassNotFoundException e) {
-                Log.e("login activity", "File contained unexpected data type: " + e.toString());
-            }
-        }
-
-        /**
-         * Save the current scoreBoard to file for later access.
-         * @param fileName name of the file to save
-         */
-
-        public void saveToFile(String fileName) {
-            try {
-                ObjectOutputStream outputStream = new ObjectOutputStream(
-                        this.openFileOutput(fileName, MODE_PRIVATE));
-                outputStream.writeObject(scoreboard);
-                outputStream.close();
-            } catch (IOException e) {
-                Log.e("Exception", "File write failed: " + e.toString());
-            }
+            serializer.saveScoreboardToFile(SCORE_FILENAME, scoreboard);
         }
 
         /**
