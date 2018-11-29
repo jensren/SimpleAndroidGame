66ee944b8be8b9f86105bd15c77fd48ecf2f0c9f
cw <wangcassidy@gmail.com>
2018-11-28 21:02:10 -0500

    Added save and load methods to Serializer

--- Phase2/GameCentre/app/src/main/java/gamecentre/Serializer.java
+++ Phase2/GameCentre/app/src/main/java/gamecentre/Serializer.java
@@ -3,7 +3,6 @@ package gamecentre;
 import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -11,6 +10,8 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+import gamecentre.cardmatching.MatchingBoard;
+import gamecentre.cardmatching.MatchingBoardManager;
 import gamecentre.slidingtiles.BoardManager;
 
 
@@ -26,7 +27,6 @@ public class Serializer extends AppCompatActivity {
      * @param fileName the name of the file
      */
     public BoardManager loadBoardManagerFromFile(String fileName) {
-
         try {
             InputStream inputStream = this.openFileInput(fileName);
             if (inputStream != null) {
@@ -69,13 +69,10 @@ public class Serializer extends AppCompatActivity {
     public Scoreboard loadScoreboardFromFile(String fileName) {
         try {
             InputStream inputStream = new FileInputStream(fileName);
-            if (inputStream != null) {
-                ObjectInputStream input = new ObjectInputStream(inputStream);
-                Scoreboard tmpScoreboard = (Scoreboard) input.readObject();
-                inputStream.close();
-                return tmpScoreboard;
-            }
-
+            ObjectInputStream input = new ObjectInputStream(inputStream);
+            Scoreboard tmpScoreboard = (Scoreboard) input.readObject();
+            inputStream.close();
+            return tmpScoreboard;
         } catch (FileNotFoundException e) {
             Log.e("login activity", "File not found: " + e.toString());
         } catch (IOException e) {
@@ -102,5 +99,82 @@ public class Serializer extends AppCompatActivity {
         }
     }
 
+    /**
+     * Load the board manager from fileName.
+     *
+     * @param fileName the name of the file
+     */
+    public MatchingBoardManager loadMatchingBoardManagerFromFile(String fileName) {
+        try {
+            InputStream inputStream = this.openFileInput(fileName);
+            if (inputStream != null) {
+                ObjectInputStream input = new ObjectInputStream(inputStream);
+                MatchingBoardManager tmpBoardManager = (MatchingBoardManager) input.readObject();
+                inputStream.close();
+                return tmpBoardManager;
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        } catch (ClassNotFoundException e) {
+            Log.e("login activity", "File contained unexpected data type: " + e.toString());
+        }
+        return null;
+    }
 
+    /**
+     * Save the board manager to fileName.
+     *
+     * @param fileName the name of the file
+     */
+    public void saveMatchingBoardManagerToFile(String fileName, MatchingBoardManager matchingBoardManager) {
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(
+                    this.openFileOutput(fileName, MODE_PRIVATE));
+            outputStream.writeObject(matchingBoardManager);
+            outputStream.close();
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    /**
+     * Load the user manager from USER_FILENAME.
+     */
+    public UserManager loadUserManagerFromFile(String filename) {
+        try {
+            InputStream inputStream = this.openFileInput(filename);
+            if (inputStream != null) {
+                ObjectInputStream input = new ObjectInputStream(inputStream);
+                UserManager tmpUserManager = (UserManager) input.readObject();
+                if (tmpUserManager == null) {
+                    tmpUserManager = new UserManager();
+                }
+                inputStream.close();
+                return tmpUserManager;
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        } catch (ClassNotFoundException e) {
+            Log.e("login activity", "File contained unexpected data type: " + e.toString());
+        }
+        return null;
+    }
+
+    /**
+     * Save the user manager to USER_FILENAME.
+     */
+    public void saveUserManagerToFile(String filename, UserManager userManager) {
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(
+                    this.openFileOutput(filename, MODE_PRIVATE));
+            outputStream.writeObject(userManager);
+            outputStream.close();
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
 }
